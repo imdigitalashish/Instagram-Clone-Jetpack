c1c2c0eb7689ec0f35923009ff133042b9d8dd99
imdigitalashish <imdigitalashish@gmail.com>
2023-03-28 17:53:09 +0530

    Added Data Class✅

--- app/src/main/java/com/example/instagramprofilepageui/ui/ProfileScreen.kt
+++ app/src/main/java/com/example/instagramprofilepageui/ui/ProfileScreen.kt
@@ -1,20 +1,26 @@
 package com.example.instagramprofilepageui.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,12 +28,13 @@ import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.instagramprofilepageui.R
+import com.example.instagramprofilepageui.StoryHighlight
 
 
 @Composable
 fun ProfileScreen() {
     Column(modifier = Modifier.fillMaxSize()) {
-        TopBar(name = "Instagram")
+        TopBar(name = "imdigitalashish")
         Spacer(modifier = Modifier.height(4.dp))
         ProfileSection()
     }
@@ -87,24 +94,35 @@ fun ProfileSection(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.fillMaxWidth(),
         ) {
-            RoundImage(image = painterResource(id = R.drawable.philipp), modifier = Modifier
-                .size(100.dp)
-                .weight(3f)
+            RoundImage(
+                image = painterResource(id = R.drawable.philipp), modifier = Modifier
+                    .size(100.dp)
+                    .weight(3f)
 
             )
             Spacer(modifier = Modifier.width(16.dp))
             StatsSection(modifer = Modifier.weight(7f))
         }
 
+        ProfileDescription(
+            displayName = "Ashish Kumar Verma",
+            description = "National Winner in web tech",
+            url = "https://imdigitalashish.netlify.app",
+            followedBy = listOf("vacoder", "username"),
+            otherCount = 17
+        )
+
+        ButtonsSection()
+
     }
 }
 
 @Composable
 fun RoundImage(
-  image: Painter,
-  modifier: Modifier = Modifier
-){
-    
+    image: Painter,
+    modifier: Modifier = Modifier
+) {
+
     Image(
         painter = image,
         contentDescription = null,
@@ -116,20 +134,19 @@ fun RoundImage(
                 shape = CircleShape
             )
             .padding(3.dp)
-            .clip(CircleShape)
-        ,
+            .clip(CircleShape),
 
-    )
+        )
 }
 
 
 @Composable
 fun StatsSection(modifer: Modifier = Modifier) {
     Row(
-        verticalAlignment =Alignment.CenterVertically,
+        verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceAround,
         modifier = modifer
-    ){
+    ) {
         ProfileStat(numberText = "601", text = "Posts")
         ProfileStat(numberText = "370", text = "Followers")
         ProfileStat(numberText = "71", text = "Following")
@@ -139,17 +156,19 @@ fun StatsSection(modifer: Modifier = Modifier) {
 
 @Composable
 fun ProfileStat(
-   numberText: String,
-   text: String,
-   modifier: Modifier = Modifier
-   
+    numberText: String,
+    text: String,
+    modifier: Modifier = Modifier
+
 ) {
-    
-    
-    Column(verticalArrangement = Arrangement.Center,
-    horizontalAlignment = Alignment.CenterHorizontally,
-    modifier = modifier) {
-        
+
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+
         Text(
             text = numberText,
             fontWeight = FontWeight.Bold,
@@ -159,7 +178,166 @@ fun ProfileStat(
         Text(text = text)
 
     }
-    
+
 }
 
 
+@Composable
+fun ProfileDescription(
+    displayName: String,
+    description: String,
+    url: String,
+    followedBy: List<String>,
+    otherCount: Int
+) {
+
+    val letterSpacing = 0.5.sp;
+
+    val lineHeight = 20.sp;
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp, vertical = 10.dp)
+
+
+    ) {
+
+        Text(
+            text = displayName,
+            fontWeight = FontWeight.Bold,
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight,
+        )
+        Text(
+            text = description,
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight,
+        )
+        Text(
+            text = url,
+            color = Color(0xFF3D3D91),
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight,
+        )
+        if (followedBy.isNotEmpty()) {
+            Text(
+                text = buildAnnotatedString {
+                    val boldStyle = SpanStyle(
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold
+                    )
+                    append("Followed By ")
+                    pushStyle(boldStyle)
+                    followedBy.forEachIndexed { index, name ->
+                        pushStyle(boldStyle)
+                        append(name)
+                        pop()
+                        if (index < followedBy.size - 1) {
+                            append(", ")
+
+                        }
+                    }
+                    if (otherCount > 2) {
+                        append(" and ")
+                        pushStyle(boldStyle)
+                        append("$otherCount others")
+                    }
+                },
+                letterSpacing = letterSpacing,
+                lineHeight = lineHeight
+            )
+
+        }
+
+    }
+
+}
+
+
+@Composable
+fun ButtonsSection() {
+
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+            border = BorderStroke(
+                width = 1.dp,
+                color = Color.LightGray
+            ),
+            shape = RoundedCornerShape(5.dp)
+
+        ) {
+            Text(
+                text = "Following",
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Black
+
+            )
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+            border = BorderStroke(
+                width = 1.dp,
+                color = Color.LightGray
+            ),
+            shape = RoundedCornerShape(5.dp)
+
+
+        ) {
+            Text(
+                text = "Message",
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Black
+
+            )
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+            border = BorderStroke(
+                width = 1.dp,
+                color = Color.LightGray
+            ),
+            shape = RoundedCornerShape(5.dp)
+
+        ) {
+            Text(
+                text = "Email",
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Black
+
+            )
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+            border = BorderStroke(
+                width = 1.dp,
+                color = Color.LightGray,
+
+            ),
+            shape = RoundedCornerShape(5.dp)
+
+        ) {
+          Icon(Icons.Default.ArrowDropDown, contentDescription = null)
+        }
+    }
+
+}
+
+
+@Composable
+fun HighlightSection(
+    modifier: Modifier = Modifier,
+    highlight: List<StoryHighlight>
+) {
+
+}
