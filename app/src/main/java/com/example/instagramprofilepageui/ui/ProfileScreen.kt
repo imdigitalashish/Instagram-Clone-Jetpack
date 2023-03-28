9862e205fb07b327cdc3fee1ab9566b6a589840c
imdigitalashish <imdigitalashish@gmail.com>
2023-03-28 13:08:39 +0530

    Profile Section Done ✅

--- app/src/main/java/com/example/instagramprofilepageui/ui/ProfileScreen.kt
+++ app/src/main/java/com/example/instagramprofilepageui/ui/ProfileScreen.kt
@@ -1,6 +1,9 @@
 package com.example.instagramprofilepageui.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -8,10 +11,14 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.instagramprofilepageui.R
@@ -21,6 +28,8 @@ import com.example.instagramprofilepageui.R
 fun ProfileScreen() {
     Column(modifier = Modifier.fillMaxSize()) {
         TopBar(name = "Instagram")
+        Spacer(modifier = Modifier.height(4.dp))
+        ProfileSection()
     }
 }
 
@@ -34,7 +43,9 @@ fun TopBar(
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceAround,
-        modifier = modifier.fillMaxWidth()
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 9.dp)
     ) {
         Icon(
             imageVector = Icons.Default.ArrowBack,
@@ -65,3 +76,90 @@ fun TopBar(
     }
 
 }
+
+@Composable
+fun ProfileSection(
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.fillMaxWidth()) {
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            RoundImage(image = painterResource(id = R.drawable.philipp), modifier = Modifier
+                .size(100.dp)
+                .weight(3f)
+
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            StatsSection(modifer = Modifier.weight(7f))
+        }
+
+    }
+}
+
+@Composable
+fun RoundImage(
+  image: Painter,
+  modifier: Modifier = Modifier
+){
+    
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = modifier
+            .aspectRatio(1f, matchHeightConstraintsFirst = true)
+            .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = CircleShape
+            )
+            .padding(3.dp)
+            .clip(CircleShape)
+        ,
+
+    )
+}
+
+
+@Composable
+fun StatsSection(modifer: Modifier = Modifier) {
+    Row(
+        verticalAlignment =Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifer
+    ){
+        ProfileStat(numberText = "601", text = "Posts")
+        ProfileStat(numberText = "370", text = "Followers")
+        ProfileStat(numberText = "71", text = "Following")
+    }
+}
+
+
+@Composable
+fun ProfileStat(
+   numberText: String,
+   text: String,
+   modifier: Modifier = Modifier
+   
+) {
+    
+    
+    Column(verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally,
+    modifier = modifier) {
+        
+        Text(
+            text = numberText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = text)
+
+    }
+    
+}
+
+
